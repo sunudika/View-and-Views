d0b591ad31c9ef685f0f63b628aa1b51322bb28d
Sunu Ilham Pradika <sunudika@gmail.com>
2019-10-29 22:17:40 +0700

    Added implementation to set Toolbar title

--- app/src/main/java/com/dicoding/picodiploma/suviewandviews/MainActivity.java
+++ app/src/main/java/com/dicoding/picodiploma/suviewandviews/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
     }
 }
